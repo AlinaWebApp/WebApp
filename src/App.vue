6bbf1e9afc29d8a3f98548e1f38b10d365f0a75c
Your Name <sinalinakit@mail.ru>
2022-02-12 22:17:46 +0300

    feat: now app displays divs numbers

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<template></template>
+<template>
+  <div>
+    <div v-for="number in numbers" :key="number">{{ number }}</div>
+  </div>
+</template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5],
+    };
+  },
+};
+</script>
 
-<style></style>
+<style scoped>
+.title {
+  font-weight: bold;
+}
+</style>
